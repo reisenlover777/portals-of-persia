4bddf883e3d132e08ddca97f82006b3eac7344ef
dugless11 <112522575+dugless11@users.noreply.github.com>
2026-01-27 15:49:42 -0700

    added portal stuff

--- PortalsofPersia-Godot/Objects/Scripts/portal.gd
+++ PortalsofPersia-Godot/Objects/Scripts/portal.gd
@@ -1,12 +1,15 @@
 extends Node2D
-var Node2D pairedPortal;
+var pairedPortal: Node2D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	match get_tree().get_node_count_in_group("Portal"):
 		3:
 			get_tree().get_first_node_in_group("Portal").queue_free();
+			get_tree().get_first_node_in_group("Portal").pairPortal = self;
+			pairedPortal = get_tree().get_first_node_in_group("Portal");
 		2:
+			pairedPortal = get_tree().get_first_node_in_group("Portal");
 			if get_tree().get_first_node_in_group("Portal").get_child(2).animation == "Purple":
 				$AnimatedSprite2D.play("Green")
 			else:
@@ -20,4 +23,6 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	body.position = pairedPortal.position
+	
 	pass # Replace with function body.
