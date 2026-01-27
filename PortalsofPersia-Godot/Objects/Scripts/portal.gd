df6136851d0255753dc581e82ec9730dd0939c99
dugless11 <112522575+dugless11@users.noreply.github.com>
2026-01-27 15:21:13 -0700

    fixed wall things

--- PortalsofPersia-Godot/Objects/Scripts/portal.gd
+++ PortalsofPersia-Godot/Objects/Scripts/portal.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var Node2D pairedPortal;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
