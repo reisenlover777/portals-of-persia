c4d670f06b3085e67aa1786cb0c60468da032891
dugless11 <112522575+dugless11@users.noreply.github.com>
2026-01-29 11:36:14 -0700

    added parallax backgrounds, added standing animation

--- PortalsofPersia-Godot/Objects/Scripts/character_body_2d.gd
+++ PortalsofPersia-Godot/Objects/Scripts/character_body_2d.gd
@@ -28,6 +28,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("Stand")
+		
 
 	move_and_slide()
 	
